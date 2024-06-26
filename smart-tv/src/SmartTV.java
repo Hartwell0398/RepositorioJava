08dc7c22221036c564da26bc3cd86a61d03572cc
ruanoliveira <ruanoliveira213@gmail.com>
2024-06-26 16:04:51 -0300

    Exercicio Smart TV

--- smart-tv/src/SmartTV.java
+++ smart-tv/src/SmartTV.java
@@ -1,5 +1,30 @@
 public class SmartTV {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    boolean ligada=false;
+    int canal=1;
+    int volume = 25;
+
+    public void mudarCanal (int novoCanal){
+        canal = novoCanal;
+    }
+    public void aumentarCanal(){
+        canal++;
+    }
+    public void diminuirCanal(){
+        canal--;
+    }
+    public void aumentarVolume(){
+        volume ++;
+        System.out.println("Aumentando volume para:" +volume);
+    }
+    public void diminuirVolume(){
+        volume --;
+        System.out.println("Diminuindo volume para:"+volume);
+    }
+    public void ligar(){
+        ligada=true;
+    }
+    public void desligar(){
+        ligada=false;
     }
 }
